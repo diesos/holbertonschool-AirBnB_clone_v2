15191d25d627577ad958e96f544998f551306d8e
Diesos <ozturk@live.Fr>
2024-07-23 17:58:26 +0200

    Update Flask routes and templates for number input and display

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web  application """
+""" Script that starts a Flask web application """
 
 from flask import Flask
 from flask import render_template
